36cc7404d068d75c66863ada147f377996a7aa9f
Alex Harvey <alexharv074@gmail.com>
2019-09-28 17:43:29 +1000

    Initial commit
    Basic go at a simple AWS Budgets module.

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "limit_amount" {
+  type        = number
+  description = "The budget limit amount"
+}
+
+variable "limit_unit" {
+  type        = string
+  description = "The budget limit unit. Default is USD"
+  default     = "USD"
+}
+
+variable "subscriber_email_addresses" {
+  type        = list(string)
+  description = "The list of email addresses of notification subscribers"
+  default     = []
+}
