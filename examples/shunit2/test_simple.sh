0749c0732104c75edd35de457344431bea729013
Alex Harvey <alexharv074@gmail.com>
2019-09-28 18:30:47 +1000

    Add tests

---
+++ examples/shunit2/test_simple.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091,SC2164,SC2154,SC2155,SC2103
+
+. shunit2/test_helper.sh
+
+testSimple() {
+  cd simple
+
+  if ! terraform apply -auto-approve ; then
+    fail "terraform did not apply"
+    startSkipping
+  fi
+
+  cd ..
+}
+
+testLimit() {
+  read -r amount unit <<< "$(
+    aws budgets describe-budgets --account-id "$AWS_ACCOUNT_ID" \
+      --query 'Budgets[0].BudgetLimit.[Amount, Unit]' --output text
+  )"
+  assertEquals "unexpected budget limit amount" "100.0" "$amount"
+  assertEquals "unexpected budget limit unit" "USD" "$unit"
+}
+
+testSubscriber() {
+  read -r subscription_type address <<< "$(
+    aws budgets describe-subscribers-for-notification --account-id "$AWS_ACCOUNT_ID" \
+      --budget-name budget --notification 'NotificationType=FORECASTED,ComparisonOperator=GREATER_THAN,Threshold=100.0' \
+      --query 'Subscribers[].[SubscriptionType, Address]' --output text
+  )"
+  assertEquals "unexpected subscription type" "EMAIL" "$subscription_type"
+  assertEquals "unexpected email address" "alex.harvey@cmd.com.au" "$address"
+}
+
+oneTimeTearDown() {
+  if [ "$NO_TEARDOWN" != "true" ] ; then
+    cd simple
+    terraform destroy -auto-approve
+    cd ..
+  fi
+}
+
+. shunit2
---
+++ examples/simple/main.tf
@@ -0,0 +1,6 @@
+module "budgets" {
+  source                     = "../../"
+  limit_amount               = 100
+  limit_unit                 = "USD"
+  subscriber_email_addresses = ["alex.harvey@cmd.com.au"]
+}
