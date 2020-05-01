9330f9f38c2030e9efa63ff43a40d4579d14ac41
Adam Durbin <adam.durbin@cmdsolutions.com.au>
2020-05-01 13:46:35 +1000

    fix backend

--- tests/backend.tf
+++ tests/backend.tf
@@ -1,10 +1,19 @@
 terraform {
   backend "s3" {
-    bucket                  = "cmdlab-sandpit2-terraform-backend"
+
+    bucket                  = "cmdlabtf-terraform-backend"
     key                     = "module-cmd-tf-aws-budgets"
     region                  = "ap-southeast-2"
-    profile                 = "idp-sandpit2"
-    dynamodb_table          = "cmdlab-tf-terraform-lock"
+    profile                 = "cmdlabtf-tfbackend"
+    dynamodb_table          = "cmdlabtf-terraform-lock"
     skip_metadata_api_check = true
+
+  # Uncomment to utilise local testing in CMD Sandpit
+    # bucket                  = "cmdlab-sandpit2-terraform-backend"
+    # key                     = "module-cmd-tf-aws-budgets"
+    # region                  = "ap-southeast-2"
+    # profile                 = "idp-sandpit2"
+    # dynamodb_table          = "cmdlab-tf-terraform-lock"
+    # skip_metadata_api_check = true
   }
 }
